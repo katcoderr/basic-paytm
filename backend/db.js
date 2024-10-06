d0e35fa9346d84d03d2c7d49a19f586895861ca1
Kartik <kkartiktummawar@gmail.com>
2024-10-06 19:02:34 +0530

    Schema Update

--- backend/db.js
+++ backend/db.js
@@ -10,8 +10,22 @@ const userSchema = mongoose.Schema({
     password : String
 })
 
+const accountSchema = mongoose.Schema({
+    userId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+        required : true
+    },
+    balance : {
+        type : Number,
+        required : true
+    }
+})
+
+const Account = mongoose.model("Account", accountSchema)
 const User = mongoose.model("User", userSchema)
 
 module.exports = {
-    User : User
+    User : User,
+    Account : Account
 }
