bb182506479ce7bed5b5a390c06b4728934fa628
Kartik <kkartiktummawar@gmail.com>
2024-10-07 23:58:26 +0530

    FE Done

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -1,10 +1,22 @@
-export const Balance = ({ value }) => {
+import axios from "axios"
+import { useState } from "react"
+
+export const Balance = () => {
+    const [balance, setBalance] = useState(0)
+    
+    axios.get("http://localhost:3000/api/v1/account/balance",{
+        headers :{
+            Authorization : localStorage.getItem("token")
+        }
+    }).then((response)=>{
+        setBalance(response.data.balance)
+    })
     return <div className="flex">
         <div className="font-bold text-lg">
             Your balance
         </div>
         <div className="font-semibold ml-4 text-lg">
-            Rs {value}
+            Rs {balance}
         </div>
     </div>
 }
